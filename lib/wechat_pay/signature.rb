df45329a24946abe1012902bc025b30f13749c02
yunwei <stillwyw@gmail.com>
2016-04-26 16:38:13 +0800

    bug fix....orrrrh should have wrote some test.

--- lib/wechat_pay/signature.rb
+++ lib/wechat_pay/signature.rb
@@ -9,7 +9,9 @@ module WechatPay
     def self.check_sign(hash, key)
       hash = hash['xml'] || hash[:xml] || hash
       return if (hash[:return_code] || hash['return_code']) == 'FAIL'
-      signature = hash.delete(:sign)
+      signature = hash[:sign] || hash['sign']
+      hash.delete(:sign)
+      hash.delete('sign')
       raise WechatPay::Error.new, "Signature does not match! The signature from wechat is #{signature} the one you got is #{sign(hash, key)}" unless signature.upcase == sign(hash, key).upcase
     end
   end
