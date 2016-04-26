3f2a860c7e301ac991cdda946114daf7268310a3
yunwei <stillwyw@gmail.com>
2016-04-26 16:34:44 +0800

    showing signature when get an error.

--- lib/wechat_pay/signature.rb
+++ lib/wechat_pay/signature.rb
@@ -9,7 +9,8 @@ module WechatPay
     def self.check_sign(hash, key)
       hash = hash['xml'] || hash[:xml] || hash
       return if (hash[:return_code] || hash['return_code']) == 'FAIL'
-      raise WechatPay::Error.new, "Signature does not match!" unless hash.delete(:sign) == sign(hash, key)      
+      signature = hash.delete(:sign)
+      raise WechatPay::Error.new, "Signature does not match! The signature from wechat is #{signature} the one you got is #{sign(hash, key)}" unless signature.upcase == sign(hash, key).upcase
     end
   end
 end
