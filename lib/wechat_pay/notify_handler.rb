d9056f3c58dfd73b255cc5171cb9df914a83b9e5
yunwei <stillwyw@gmail.com>
2016-05-04 15:36:20 +0800

    ok fixed a notification handling bug. probably.

--- lib/wechat_pay/notify_handler.rb
+++ lib/wechat_pay/notify_handler.rb
@@ -7,8 +7,7 @@ module WechatPay
       @hash = Hash.from_xml(post_data)
       key = WechatPay::Config.key
       WechatPay::Signature.check_sign(@hash, key)
-      
-      @result = OpenStruct.new(@hash) 
+      @result = OpenStruct.new(@hash['xml']||@hash[:xml]) 
     end
     
   end
