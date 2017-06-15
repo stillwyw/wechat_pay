15ae8c8b09b8537fe34b9bcc1f6f2fd1b4e5b210
Du Zhigang <dulao5@users.noreply.github.com>
2017-06-15 16:44:10 +0900

    fix WechatPay::Request HTTP headers

--- lib/wechat_pay/request.rb
+++ lib/wechat_pay/request.rb
@@ -17,7 +17,7 @@ module WechatPay
       headers = WechatPay::Config.http_headers.dup || {}
       headers[:content_type] = 'application/xml'
       
-      requret = HTTP.with_headers(user_agent: "dzg", content_type: 'application/xml')
+      requret = HTTP.with_headers(headers)
       self.parse(requret.post(@url, body: @out_xml))
     end
     
